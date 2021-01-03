41c4f894142d465354a04463e6d0d7f354423506
Peter Thompson <pete@thompsonsonline.net>
2021-01-03 20:37:11 +0000

    03_01_2021 - Daily Updates - 5

---
+++ ConvexCodeExamplesExternal/src/main/JS/http/consumeC114.js
@@ -0,0 +1,31 @@
+
+var https = require('https');
+
+var headers = {
+  Authorization: 'Bearer 01OZLElajKpQJonw'
+};
+
+var options = {
+  host: 'http.convexglobal.io',
+  port: 443,
+  path: '/stream/2cc2aa',
+  method: 'GET',
+  headers: headers
+};
+
+var req = https.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+// write data to request body
+//req.write('Hello World from HTTP\n');
+req.end();
